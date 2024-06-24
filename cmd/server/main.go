a9d23860fbdad17417eb61c10fd810be74940398
Philipp Muens <philipp@muens.io>
2024-06-24 18:15:42 +0200

    Rename assets to docs
    For GitHub Pages compatibility.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	fmt.Println("Starting server at http://127.0.0.1:3000")
 	// TODO: Update to use path to `assets` relative to CWD.
-	err := http.ListenAndServe(":3000", http.FileServer(http.Dir("./assets")))
+	err := http.ListenAndServe(":3000", http.FileServer(http.Dir("./docs")))
 	if err != nil {
 		fmt.Println("Failed to start server", err)
 		return
