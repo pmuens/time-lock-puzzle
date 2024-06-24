d5f33e3e4089f6c0c1895257e249c2dd4b8629c1
Philipp Muens <philipp@muens.io>
2024-06-24 16:00:19 +0200

    Add JSON formatter WebAssembly example

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
 	fmt.Printf("Hello %s\n", "World")
+
+	js.Global().Set("prettyJSON", jsonWrapper())
+
+	<-make(chan struct{})
+}
+
+func prettyJSON(input string) (string, error) {
+	var raw any
+	if err := json.Unmarshal([]byte(input), &raw); err != nil {
+		return "", err
+	}
+
+	pretty, err := json.MarshalIndent(raw, "", "  ")
+	if err != nil {
+		return "", err
+	}
+
+	return string(pretty), nil
+}
+
+func jsonWrapper() js.Func {
+	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) != 1 {
+			return "Invalid number of arguments passed."
+		}
+
+		input := args[0].String()
+		fmt.Printf("Input: %s\n", input)
+
+		prettified, err := prettyJSON(input)
+		if err != nil {
+			fmt.Printf("Unable to convert to json %s\n", err)
+			return err.Error()
+		}
+
+		return prettified
+	})
+
+	return jsonFunc
 }
